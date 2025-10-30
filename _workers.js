4e3fed2b927441605f41150c0fb56f9ef6d019e7
Merab Tato Kutalia <tatocaster@users.noreply.github.com>
2025-10-30 09:07:10 +0400

    Refactor fetch function to handle multiple paths

--- _workers.js
+++ _workers.js
@@ -1,21 +1,35 @@
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    
-    if (url.pathname === '/rss_feed.xml') {
-      const object = await env.PODCAST_BUCKET.get('rss_feed.xml');
+    const pathname = url.pathname;
+
+    const isR2Path =
+      pathname === '/rss_feed.xml' ||
+      pathname.startsWith('/audio/') ||
+      pathname.endsWith('.jpg');
+
+    if (isR2Path) {
+      // Get the object key from the path by removing the leading slash.
+      const key = pathname.substring(1);
+
+      const object = await env.PODCAST_BUCKET.get(key);
+
       if (object === null) {
         return new Response('Object Not Found', { status: 404 });
       }
+
       const headers = new Headers();
       object.writeHttpMetadata(headers);
       headers.set('etag', object.httpEtag);
-      headers.set('content-type', 'application/xml; charset=utf-8');
+
+      if (pathname === '/rss_feed.xml') {
+        headers.set('content-type', 'application/xml; charset=utf-8');
+      }
+      
       return new Response(object.body, {
         headers,
       });
     }
-
     return env.next();
   },
 };
